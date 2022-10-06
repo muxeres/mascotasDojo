ec8e612a6658b6f0ddc0b8679e53a1c278b94f32
muxeres <49755046+muxeres@users.noreply.github.com>
2022-10-06 19:40:17 -0300

    ultimo cambio

--- dojo_pets.py
+++ dojo_pets.py
@@ -32,13 +32,12 @@ class Ninja(Mascota):
         super().__init__( mascota, tipo, comida_mascota )
         self.nombre=nombre
         self.apellido=apellido
-                	
+
     def pasear(self): 
-        #walk the ninja's pet by invoking the play() pet method
+        
         self.jugar() 
     def alimentar(self): 
-        #feed the ninja's pet by invoking the eat() pet method 
+        
         self.comer()
     def bañar(self): 
-        #clean the ninja's pet by invoking the pet method sound()
-        self.sonido()
+        
